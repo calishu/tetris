d22ec4f6f4ed1e00dcaa3f4d0c5c518f162c5286
calishu <danny.kretschmann@proton.me>
2025-09-07 02:17:45 +0200

    feat: added framebuffer

---
+++ main/graphics/framebuffer.c
@@ -0,0 +1,31 @@
+#include "config.h"
+
+#include <stdlib.h>
+
+int **framebuffer_init()
+{
+    int **framebuf = malloc(DISPLAY_SIZE_HEIGHT * sizeof(int *));
+    if (!framebuf) return NULL;
+
+    for (size_t y = 0; y < DISPLAY_SIZE_HEIGHT; ++y)
+    {
+        framebuf[y] = calloc(DISPLAY_SIZE_WIDTH, sizeof(int));
+
+        if (!framebuf[y])
+        {
+            for (size_t i = 0; i < y; ++i)
+                free(framebuf[i]);
+            free(framebuf);
+            return NULL;
+        }
+    }
+
+    return framebuf;
+}
+
+void framebuffer_free(int **framebuf)
+{
+    for (size_t i = 0; i < DISPLAY_SIZE_HEIGHT; ++y)
+        free(framebuf[i]);
+    free(framebuf);
+}
